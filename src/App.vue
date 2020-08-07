baae81e5fb1571073ab33d2a0166c49731539f58
pepenet <pepenet@pepe.com>
2020-08-07 20:10:41 -0300

    1ercommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="flexbox">
+hello
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+margin:0;
+padding:0;
+box-sizing: border-box;
+}
+body{
+background-color: #f3f3f3;
+}
+.flexbox{
+display:flex;
+justify-content: space-between;
+width: 100%;
+max-width:768px;
+height: 100vh;
+overflow:hidden;
+margin: 0 auto;
+padding: 15px;
+
+}
+.flexbox .board {
+display: flex;
+flex-direction:column;
+width:100%;
+max-width: 900px;
+background-color:#313131;
+padding: 15px;
+}
+
+.flexbox .board .card {
+padding: 15px 25px;
+background-color:#f3f3f3;
+cursor: pointer;
+margin-bottom: 15px;
 }
 </style>
